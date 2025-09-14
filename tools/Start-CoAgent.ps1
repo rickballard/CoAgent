cde2b47d826841bb1621bf6887af4fd2f38f62fd
Rick Ball <41898282+rickballard@users.noreply.github.com>
2025-09-14 03:13:29 -0400

    tools: Start-CoAgent launcher (bind panes, start electron)

---
+++ tools/Start-CoAgent.ps1
@@ -0,0 +1,10 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+# Bind panes
+$env:COAGENT_CHAT_URL = 'data:text/html,<h2>Chat</h2><p>connected</p>'
+$env:COAGENT_OPS_URL  = 'data:text/html,<h2>Ops</h2><ul><li>Ready</li></ul>'
+$env:COAGENT_EXEC_URL = 'http://127.0.0.1:7681'
+
+# Launch Electron (dev)
+Start-Process -FilePath npm -WorkingDirectory (Join-Path $PSScriptRoot '..\electron') -ArgumentList 'start'
